67bf358f9a8d2beee4467a039957a9380790f427
OlgaFimbresMorales <olgamafimbresmrls@gmail.com>
2015-04-12 21:00:55 -0700

    Producto6

--- Producto6/parabolico.f90
+++ Producto6/parabolico.f90
@@ -1,13 +1,12 @@
 Program Tiro_p
 Implicit None
-real, parameter :: g = 9.81, p =1.1644, pi = 4.0*atan(1.0)
-real :: dt, x0, y0, v0, v0x, v0y, ax, ay, m, CD, dt_r, FD, x, y, n, A 
-integer :: t
+real, parameter :: g = 9.81, p =1.1644, pi = 4.0*atan(1.0), CD = 0.47
+real :: dt, x0, y0, v0, v0x, v0y, ax, ay, m, dt_r, FD, x, y, t, tv, A, r, D, ty
+integer :: n
 write (*,*) 'Ingrese la masa del proyectil'
 read *, m
-write (*,*) 'Elija un coeficiente de fricción:'
-write (*,*) 'Esfera: 0.47, Media esfera: 0.42, Cono: 0.5, Cubo: 1.05, Cilindro: 0.82, Aerodinámico: 0.04'
-read *, CD
+write (*,*) 'Identifique el radio del proyectil'
+read *, r
 write (*,*) 'Ingrese una velocidad inicial para el proyectil'
 read *, v0
 write (*,*) 'Ingrese un ángulo inicial para el proyectil'
@@ -18,25 +17,49 @@ write (*,*) 'Determine una posición inicial en el eje y'
 read *, y0
 
 
+
 dt_r =(dt*pi)/180
-v0x = v0*cos(dt_r)
-v0y = v0*sin(dt_r)
-FD = (1/2)*p*v0*v0*CD*A
+v0x = v0*cos(dt_r) !velocidad inicial en x
+v0y = v0*sin(dt_r) !velocidad inicial en y
+A = pi*r*r !area transversal
+FD = D*v0*v0 !fuerza de arrastre 
+D = p*CD*A*0.5
+n = (v0y + sqrt((v0y*v0y) + (2*g*y0)))/g !tiempo total de vuelo
+call Tiro_a(m, p, A, D, v0, v0x, v0y, ax, ay, g)
+ty = -v0y/ay !tiempo altura maxima
 
-n = (v0y + sqrt((v0y*v0y) + (2*g*y0)))/g
-call Tiro_d (v0, v0y, v0x, x0, y0, m, CD, FD, n, g, x, y, t)
+tv = sqrt(m/(g*D))*acos(y)*(exp((y*D)/m)) !tiempo en caer
 
-print * , 'Tiempo total de vuelo',t
+print * , 'tiempo subir', ty
+print * , 'tiempo caer', tv
+
+print * , 'ay =', ay
+print * , 'ax =', ax
+print * , 'D=', D
+
+call Tiro_d(dt_r, x0, y0, v0x, v0y, ax, ay, t, x, y, g, ty, tv, m, D)
+
+print * , 'Tiempo total de vuelo', t
 print * , 'Distancia máxima recorrida', x
 print * , 'Altura máxima alcanzada', y
 
 End Program Tiro_p  
 
-Subroutine Tiro_d (v0, v0y, v0x, x0, y0, m, CD, FD, n, g, x, y, t)
-real, intent(in) :: v0, v0y, v0x, x0, y0, m, CD, FD, n, g
-real, intent(inout) :: x, y
-   integer, intent(inout) :: t
-t = (2/FD)*log(1+((FD*v0y)/g))
-y = (v0y*t) + ((m/FD)*(Log(cos(sqrt((g*FD)/m)*t))))
-x = (v0x*t) + ((m/FD)*(Log(t)))
+Subroutine Tiro_d(dt_r, x0, y0, v0x, v0y, ax, ay, t, x, y, g, ty, tv, m, D)
+real, intent(in) :: dt_r, x0, y0, v0x, v0y, ax, ay, g, tv, ty, m, D
+real, intent(inout) :: t, x, y
+
+t = ty+tv
+x = x0 + (v0x*t) + ((m/D)*log(t))
+y = (v0y*ty) + ((1/2)*ay*ty*ty)
+
 end subroutine Tiro_d
+
+Subroutine Tiro_a(m, p, A, D, v0, v0x, v0y, ax, ay, g)
+real, intent(in) :: m, p, A, D, v0, v0x, v0y, g
+real, intent(inout) :: ax, ay
+
+ax = -(D/m)*v0*v0x
+ay = -g-(D*v0*v0y*(1/m))
+
+end subroutine Tiro_a
