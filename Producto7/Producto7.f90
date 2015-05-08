07d496b7d8e5f270c2b14552df96311267d1427f
ita <olgamafimbresmrls@gmail.com>
2015-05-07 20:03:52 -0700

    Producto 7

--- Producto7/Producto7.f90
+++ Producto7/Producto7.f90
@@ -1,70 +1,208 @@
 Module periodos
 implicit none
-integer, parameter :: datos=7674, mes=1344, dias=48
+integer, parameter :: datos=7632, mes=1344, dias=159, dia=48, mediodia=24, mesesc=5376!datos en los meses completos
 end module periodos
 
 Program Mareas
 use periodos
 Implicit None
-real :: p, t, d, mx, im, maximo, minimo, dia, minm
-real, dimension(datos)::B, altura, tiempo, tiem, maxm
-integer :: i
+real :: p, t, d, mx, im, maximo, minimo, minimod, maximod, maximd
+real, dimension(1:datos):: B, altura, tiempo, tiem, maxm, min
+real, dimension(1:5):: mm, tm, periodomaxm,  periodominm
+real, dimension(1:dias)::  periodomaxd, periodomind
+real, dimension(1:318):: periodomd
+integer :: i, j
 
 
 
-open(unit=8, file='Mareas5.csv',status='old')
+open(unit=8, file='Mareas.csv',status='old')
 do i=1, datos, 1 
    read (8,*) B(i), p, t, altura(i), d, tiempo(i)
-   !write (*,*) B(i),p, t, altura(i), d, tiempo(i)
+  !write (*,*) B(i),p, t, altura(i), d, tiempo(i)
 end do  
+
+print * , '-------------------------------'
+print * , 'Niveles máximos por mes'
+open(unit=10, file='max.csv', status='unknown')
+do j=0, mesesc, mes
+maximo = -1
+  do i=1,mes,1
+     if(altura(i+j).gt.maximo) then
+     maximo = altura(i+j)
+     mx = tiempo(i+j)
+     end if
+     continue
+  end do
+
+      write(10,*) mx, maximo
+     ! write(*,*) mx, maximo
+end do  
+close(10)
+
+
+print * , 
+print * , '-------------------------------'
+print * , 'Niveles minimos por mes'
+open(unit=11,file='min.csv',status='unknown')
+
+
+do j=0, mesesc, mes
+minimo=0
+  do i=1,mes,1
+   if(altura(i+j).lt.minimo) then
+   minimo = altura(i+j)
+   im = tiempo(i+j)
+   end if
+  end do
+  write(11,*) im, minimo
+   !write(*,*) im, minimo
+end do
+close(11)
+
+print * , '--------------------------------'
+print * , '!Mínimos diarios'
+open (unit=12, file='mindia.csv', status='unknown')
+do j=0,datos-1,dia
+minimod=0
+  do i=1,dia,1
+  if(altura(i+j).lt.minimod) then
+  minimod = altura(i+j)
+  im = tiempo(i+j) 
+  end if
+  end do
+  write (12,*) im, minimod
+  !write (*,*) im, minimod
+end do
+close(12)
+
+
+print * , '--------------------------------'
+print * ,'!Máximos diarios'
+open (unit=13, file='maxdia.csv', status='unknown')
+do j=0,datos-1,dia
+maximod=-1
+  do i=1,dia,1
+  if(altura(i+j).gt.maximod) then
+  maximod = altura(i+j)
+  im = tiempo(i+j)
+  end if
+  end do
+  write (13,*) im, maximod
+ !write (*,*) im, maximod
+end do
+close(13)
 close (8)
 
-open(unit=8, file='Mareas5.csv', status='old')
-open(9, file='maximos.csv', status='unknown')
-mx=0
-do i=1, datos
-  read (8,*) B(i), p, t, altura(i), d, tiempo(i)
-  if(altura(i).gt.mx) then
-    mx = altura(i)
-    im = tiempo(i)
-  end if
+
+
+print * , 'nivel de marea máxima', maximo
+print * , 'mivel de marea mímina', minimo
+print * , 'tiempo de marea mínima', dia
+
+print * , '-------------------------------'
+print * , 'Periodos entre máximos por mes'
+open(unit=10, file='max.csv', status='unknown')
+
+do i=1,5, 1
+   read(10,*) tm(i), mm(i)
+ if (i .gt. 1) then
+   periodomaxm(i) = tm(i) - tm(i-1)
+ end if
+   !write(*,*) periodomaxm(i)
+  
 end do
-write(9,*) mx, im
-close(9)
-close(8)
-
-print * , 'nivel de marea maxima', mx, 'metros'
-print * , 'tiempo de marea maxima', im
-
-open(unit=8, file='Mareas5.csv', status='old')
-open(10, file='maxymin.csv',status='unknown')
-
- do i=1,datos
-   read (8,*) B(i), p, t, altura(i), d, tiempo(i)
-   minimo = minval(altura,1)
-   dia = tiempo(i)
- end do
- write(10,*) dia, minimo
- write(*,*) dia, minimo
+  !write(*,*) periodomaxm
+  print * , 'Periodo promedio entre maximos mensuales'
+  write(*,*) sum(periodomaxm)/4, 'dias'
 close(10)
-close(8)
 
-open(unit=8, file='Mareas5.csv', status='old')
-open(11, file='max2.csv',status='unknown')
-maxm=mx
- do i=1, datos
-   read (8,*) B(i), p, t, altura(i), d, tiempo(i)
-    
-   
- end do
- write(11,*) maxm
- write(*,*) maxm
+print * , '-------------------------------'
+print * , 'Periodos entre mínimos por mes'
+open(unit=11,file='min.csv',status='unknown')
+do i=1,5,1
+  read(11,*) tm(i), mm(i)
+  periodominm(i) = tm(i) - tm(i-1)
+  !write(*,*) periodominm(i)
+end do
+  !write(*,*) periodominm(i)
 close(11)
+
+print * , '-------------------------------'
+print * , 'Periodos entre mínimos por dias'
+open (unit=12, file='mindia.csv', status='unknown')
+do i =1,159,1
+   read(12,*) tm(i), mm(i)
+   periodomind(i) = tm(i) - tm(i-1)
+  !write(*,*) periodomind(i)
+end do
+  !write(*,*) periodomind(i)
+close(12)
+
+print * , '-------------------------------'
+print * , 'Periodos entre máximos por dias'
+open (unit=13, file='maxdia.csv', status='unknown')
+do i=1,dias,1
+   read(13,*) tm(i), mm(i)
+if (i .gt. 1) then
+   periodomaxd(i) = tm(i)-tm(i-1)
+end if
+   !write(*,*) periodomaxd(i)
+end do
+   print * , '-------------------------------'
+   print * , 'Periodo promedio entre maximos diarios'
+  !write(*,*) sum(periodomaxd)/(dias-1), 'dias'
+close(13)
+
+print * , '-------------------------------'
+
+print * , 'mareas semidiurnas'
+open(unit=8, file='Mareas.csv',status='old')
+do i=1, datos, 1 
+   read (8,*) B(i), p, t, altura(i), d, tiempo(i)
+  !write (*,*) B(i),p, t, altura(i), d, tiempo(i)
+end do  
+
+
+open (unit=14, file='semidiurna.csv', status='unknown')
+
+do i=1,318,1
+   read(14,*) tm(i),mm(i)
+end do
+
+do j=0,datos-1,mediodia
+ maximd=-1
+ do i=1,mediodia,1
+  if(altura(i+j).gt.maximd) then
+  maximd = altura(i+j)
+  im = tiempo(i+j)
+  end if
+ end do
+ 
+  write (14,*) im, maximd
+  !write (*,*) im, maximd
+end do
+
+print * , '-------------------------------'
+print * , 'diferencias entre mareas semidiurnas'
+!promedio semidiurnas
+do i=1,318,1
+ 
+  if (i .gt. 1) then
+   periodomd(i) = tm(i)-tm(i-1)
+  end if
+  write(*,*) periodomd(i)
+end do
+   print * , '-------------------------------'
+   print * , 'Periodo promedio entre mareas semidiurnas'
+  write(*,*) sum(periodomd)/317, 'dias' 
+
+close(14)
 close(8)
 
 
 
 end Program Mareas
+
 
 
 
--- Producto7/periodos.mod
+++ Producto7/periodos.mod
@@ -1,5 +1,5 @@
 GFORTRAN module version '10' created from Producto7.f90
-MD5:d09fb9d331f10238fca599922316d83b -- If you edit this, you'll get what you deserve.
+MD5:eeb60ca899448fe907f857e4254b9783 -- If you edit this, you'll get what you deserve.
 
 (() () () () () () () () () () () () () () () () () () () () () () () ()
 () () ())
@@ -16,16 +16,26 @@ MD5:d09fb9d331f10238fca599922316d83b -- If you edit this, you'll get what you de
 
 (2 'datos' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
 UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
-CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '7674') () 0 () () () 0 0)
-3 'dias' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '7632') () 0 () () () 0 0)
+3 'dia' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
 IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (CONSTANT (
 INTEGER 4 0 0 0 INTEGER ()) 0 '48') () 0 () () () 0 0)
-4 'mes' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+4 'dias' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (CONSTANT (
+INTEGER 4 0 0 0 INTEGER ()) 0 '159') () 0 () () () 0 0)
+5 'mediodia' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '24') () 0 () () () 0 0)
+6 'mes' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
 IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (CONSTANT (
 INTEGER 4 0 0 0 INTEGER ()) 0 '1344') () 0 () () () 0 0)
-5 'periodos' 'periodos' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC
+7 'mesesc' 'periodos' '' 1 ((PARAMETER UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () (
+CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '5376') () 0 () () () 0 0)
+8 'periodos' 'periodos' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC
 UNKNOWN UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0
 0)
 )
 
-('datos' 0 2 'dias' 0 3 'mes' 0 4 'periodos' 0 5)
+('datos' 0 2 'dia' 0 3 'dias' 0 4 'mediodia' 0 5 'mes' 0 6 'mesesc' 0 7
+'periodos' 0 8)
